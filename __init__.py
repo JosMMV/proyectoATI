daeb93a64a6ec765ba0b9c483a76d63b0dfd995c
JosMMV <jsmiguel15@hotmail.com>
2018-06-28 10:38:15 -0400

    primer commit

---
+++ __init__.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request
+from pymongo import *
+ 
+app = Flask(__name__, template_folder = 'templates', static_folder = 'static')
+
+# MongoDB Connection with PyMongo
+client = MongoClient()
+
+db = client.proyectoATI
+usuarios = db.user
+imagenes = db.image
+
+# Routes Definition
+@app.route('/')
+def index():
+	users = usuarios.aggregate([
+	{"$match":
+		{
+			"conf": True
+		}
+
+	},
+  {"$lookup":
+    {
+      "from": "image",
+      "localField": "images._id_image",
+      "foreignField": "_id_image",
+      "as": "user_images"
+    }
+  }
+	])
+	return render_template('login.html', usuarios = users)
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+#@app.route('/register', methods = ['POST'])
+#def register():
+	
+
+@app.route('/home', methods=['POST'])
+def home():
+	user = request.form['user']
+	password = request.form['pass']
+
+	usr = usuarios.find_one({ "user_name": user })
+	if usr["password"] == password:
+		users = usuarios.aggregate([
+		{"$lookup":
+			{
+				"from": "image",
+				"localField": "images._id_image",
+				"foreignField": "_id_image",
+				"as": "user_images"
+			}
+		}
+		])
+		return render_template('index.html', usr = usr, loged = True, usuarios = users)
+	return render_template('login.html', error = True)
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
